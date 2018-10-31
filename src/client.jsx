25df962623a8770c778e57b340a7ccf4e3b293e0
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2018-10-31 21:51:46 +0100

    Added styling

--- src/client.jsx
+++ src/client.jsx
@@ -2,22 +2,34 @@ import { h, render, Component } from 'preact'
 import axios from 'axios'
 
 function SubmittingNotification(props) {
-  return <div>Submitting form!</div>
+  return (
+		<div>
+			Submitting form!
+		</div>
+	)
 }
 
 function SubmittedNotification(props) {
-  return <div>Submitted form!</div>
+  return (
+		<div>
+			Thanks for signing up!
+		</div>
+	)
 }
 
 function Form(props) {
   return (
-    <div>
-      <label>Email: </label>
-      <input type="text" id="email-input" 
-        onChange={props.emailObserver} />
-      <button
-        onClick={props.submitHandler}>Submit</button>
-    </div>
+		<div>
+			<p>[event description]</p>
+			<p>Write your email below and click submit to sign up for the event!</p>
+			<p>The email will be removed from our database on the day after the event.</p>
+			<label>Email:
+			<input type="text" id="email-input" 
+				onChange={props.emailObserver} />
+			<button
+				onClick={props.submitHandler}>Submit</button>
+			</label>
+		</div>
   )
 }
 
@@ -38,12 +50,6 @@ class App extends Component {
   }
 
   render() {
-    if (this.state.state === this.states.WAITING) {
-      return <SubmittingNotification/>
-    } else if (this.state.state === this.states.SUBMITTED) {
-      return <SubmittedNotification/>
-    }
-
     const emailObserver = e => {
       this.setState({email: e.target.value})
     }
@@ -56,7 +62,25 @@ class App extends Component {
       })
     }
 
-    return <Form emailObserver={emailObserver} submitHandler={submitHandler}/>
+		let main = <Form emailObserver={emailObserver} submitHandler={submitHandler}/>
+
+    if (this.state.state === this.states.WAITING) {
+      main = <SubmittingNotification/>
+    } else if (this.state.state === this.states.SUBMITTED) {
+      main = <SubmittedNotification/>
+    }
+
+		return (
+			<div>
+				<header>
+					<h1>Signup form for [event title]</h1>
+					<img src="brainpuzzle.gif"/>
+				</header>
+				<main>
+					{main}
+				</main>
+			</div>
+		)
   }
 }
 
